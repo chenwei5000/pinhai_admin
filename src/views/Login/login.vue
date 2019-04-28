ce6252d467fd9f790ac80aec528dbd824878cb62
tankai <tank@1ycloud.net>
2019-04-28 13:07:12 +0800

    重构登陆注册表单验证

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -1,44 +1,53 @@
 <template>
 
-  <body class="login-bg">
-  <div class="login">
-    <div class="message">
-      <img src="../../assets/logo-2.png"/>
-      {{global.config.NAME}}
-      <small>{{global.config.VERSION}}</small>
+  <div class="login-bg">
+    <div class="login">
+      <div class="message">
+        <img src="../../assets/logo-2.png"/>
+        {{global.config.NAME}}
+        <small>{{global.config.VERSION}}</small>
+      </div>
+      <div id="darkbannerwrap"></div>
+
+      <form method="post" :model="user" class="layui-form">
+
+        <input name="username" v-model="user.name" placeholder="用户名" type="text" lay-verify="required|email"
+               class="layui-input"/>
+
+        <hr class="hr15">
+        <input name="password" v-model="user.pass" lay-verify="required" placeholder="密码" type="password"
+               class="layui-input"/>
+
+        <hr class="hr15">
+
+        <input class="loginin" value="登录" @click="login" lay-submit lay-filter="login" style="width:100%;"
+               type="button"/>
+        <hr class="hr20">
+      </form>
+
     </div>
-    <div id="darkbannerwrap"></div>
-
-    <form method="post" :model="user" class="layui-form">
-
-      <input name="username" v-model="user.name" placeholder="用户名" type="text" lay-verify="required"
-             class="layui-input"/>
-
-      <hr class="hr15">
-      <input name="password" v-model="user.pass" lay-verify="required" placeholder="密码" type="password"
-             class="layui-input"/>
-
-      <hr class="hr15">
-
-      <input class="loginin" value="登录" @click="login" lay-filter="login" style="width:100%;" type="button"/>
-      <hr class="hr20">
-    </form>
-
   </div>
-  </body>
 </template>
 
 <script>
 
   import systemMode from '../../models/system.js'
 
+  layui.use(['form']);
+
   export default {
     methods: {
       login() {
-        var loginParams = {account: this.user.name, password: this.user.pass};
-        systemMode.login(loginParams).then(token => {
-          this.$store.commit("setToken", token);
-          this.$router.replace('/');
+        var _this = this;
+        layui.use(['form'], function () {
+          var form = layui.form;
+          form.on('submit(login)', function (data) {
+            var loginParams = {account: _this.user.name, password: _this.user.pass};
+            systemMode.login(loginParams).then(token => {
+              _this.$store.commit("setToken", token);
+              _this.$router.replace('/');
+            });
+          });
         });
       }
     },
@@ -50,7 +59,7 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style type="text/less" lang="scss" scoped>
 
   @import "../../assets/css/phadmin.less";
 
