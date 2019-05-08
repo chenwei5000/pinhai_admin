9ae4e2f82fb7c9ae6414d1c02c6628daeaec79c0
tankai <tank@1ycloud.net>
2019-05-08 18:15:20 +0800

    整理货币列表页面

--- src/views/Currency/index.vue
+++ src/views/Currency/index.vue
@@ -5,11 +5,7 @@
       </ph-card-header>
       <div class="ph-card-body">
         <ph-table
-          :url="url"
-          :columns="columns"
-          :searchForm="searchForm"
-          :form="form"
-          :tableAttrs="tableAttrs"
+          v-bind="tableConfig"
         >
         </ph-table>
       </div>
@@ -23,203 +19,241 @@
   export default {
     data() {
       return {
-        title: '货币管理', 
-        url: '/currencies', 
-        tableAttrs: {
-          stripe: true,
-          border: true,
-          "row-class-name": this.statusClassName,
-          "highlight-current-row": true
-        },
-        columns: [
-          {type: 'selection'}, 
-          {prop: 'id', label: 'ID', sortable: 'custom', hidden: true},
-          {prop: 'name', label: '名称', sortable: 'custom'},
-          {prop: 'code', label: '代码'},
-          {prop: 'symbolLeft', label: '左侧符'},
-          {prop: 'symbolRight', label: '右侧符'},
-          {prop: 'decimalPoint', label: '小数符'},
-          {prop: 'thousandsPoint', label: '千分位符'},
-          {prop: 'decimalPlaces', label: '小数位数'},
-          {prop: 'value', label: '汇率值', sortable: 'custom'},
-          {
-            prop: 'status',
-            label: '状态',
-            formatter: row => (row.status === 1 ? '启用' : '禁用')
-          },
-          {
-            prop: 'lastModified',
-            label: '最后修改时间',
-            formatter: row => {
-              return parseTime(row.lastModified, '{y}-{m}-{d} {h}:{i}');
-            }
-          }
-        ],
+        title: '货币管理',
 
-        // 搜索区块定义
-        searchForm: [
-          {
-            $type: 'input',
-            $id: 'name',
-            label: '名称',
-            $el: {
-              op: 'bw',
-              placeholder: '请输入名称'
+        tableConfig: {
+          url: '/currencies',
+          tableAttrs: {
+            "row-class-name": this.statusClassName
+          },
+          columns: [
+            {type: 'selection'},
+            {prop: 'id', label: 'ID', sortable: 'custom', hidden: true},
+            {prop: 'name', label: '名称', sortable: 'custom', 'min-width': 100, fixed: 'left'},
+            {prop: 'code', label: '代码', sortable: 'custom', width: 80},
+            {prop: 'symbolLeft', label: '左侧符', width: 80},
+            {prop: 'symbolRight', label: '右侧符', width: 80},
+            {prop: 'decimalPoint', label: '小数符', width: 80},
+            {prop: 'thousandsPoint', label: '千分位符', width: 80},
+            {prop: 'decimalPlaces', label: '小数位数', width: 80},
+            {prop: 'value', label: '汇率值', sortable: 'custom', width: 100},
+            {
+              prop: 'status',
+              label: '状态',
+              width: 80,
+              formatter: row => (row.status === 1 ? '启用' : '禁用')
+            },
+            {
+              prop: 'lastModified',
+              label: '最后修改时间',
+              'width': 140,
+              formatter: row => {
+                return parseTime(row.lastModified, '{y}-{m}-{d} {h}:{i}');
+              }
             }
-          },
-          {
-            $type: 'input',
-            $id: 'code',
-            label: '代码',
-            $el: {
-              op: 'bw',
-              placeholder: '请输入代码'
-            }
-          }
-        ],
-        //  弹窗表单, 用于新增与修改
-        form: [
-          {
-            $type: 'input',
-            $id: 'name',
-            label: '名称',
-            $el: {
-              placeholder: '请输入名称'
-            },
-            rules: [
-              {
-                required: true,
-                message: '请输入名称',
-                trigger: 'change'
-              }
-            ]
-          },
-          {
-            $type: 'input',
-            $id: 'code',
-            label: '代码',
-            $el: {
-              placeholder: '请输入代码'
-            },
-            rules: [
-              {
-                max: 3,
-                required: true,
-                message: '请输入三位代码',
-                trigger: 'blur'
-              }
-            ]
-          },
-          {
-            $type: 'input',
-            $id: 'symbolLeft',
-            label: '左侧符',
-            $el: {
-              placeholder: '请输入左侧符'
-            },
-            rules: [
-              {
+          ],
 
-                message: '请输入左侧符',
-                trigger: 'blur'
+          // 搜索区块定义
+          searchForm: [
+            {
+              $type: 'input',
+              $id: 'name',
+              label: '名称',
+              $el: {
+                op: 'bw',
+                placeholder: '请输入名称'
               }
-            ]
-          },
-          {
-            $type: 'input',
-            $id: 'symbolRight',
-            label: '右侧符',
-            $el: {
-              placeholder: '请输入右侧符'
             },
-            rules: [
-              {
-                message: '请输入右侧符',
-                trigger: 'blur'
+            {
+              $type: 'input',
+              $id: 'code',
+              label: '代码',
+              $el: {
+                op: 'bw',
+                placeholder: '请输入代码'
               }
-            ]
-          },
-          {
-            $type: 'input',
-            $id: 'decimalPoint',
-            label: '小数符',
-            $default: '.',
-            $el: {
-              placeholder: '请输入小数符'
             },
-            rules: [
-              {
-                max: 1,
-                message: '请输入小数符',
-                trigger: 'blur'
-              }
-            ]
-          },
-          {
-            $type: 'input',
-            $id: 'thousandsPoint',
-            label: '千分位符',
-            $default: ',',
-            $el: {
-              placeholder: '请输入千分位符'
-            },
-            rules: [
-              {
-                max: 1,
-                message: '请输入千分位符',
-                trigger: 'blur'
-              }
-            ]
-          },
-          {
-            $type: 'input',
-            $id: 'decimalPlaces',
-            label: '小数位数',
-            $default: 2,
-            $el: {
-              placeholder: '请输入小数位数',
-            },
-            rules: [
-              {
-                whitespace: true,
-                type: 'number',
-                transform(value) {
-                  if (value) {
-                    return Number(value);
-                  }
-                },
-                message: '小数位数必须为正整数',
-              }
-            ]
-          },
-          {
-            $type: 'input',
-            $id: 'value',
-            label: '汇率值',
-            $default:'1' ,
-            $el: {
-              placeholder: '请输入汇率值'
-            },
-
-          },
-          {
-            $type: 'radio-group',
-            $id: 'status',
-            label: '状态',
-            $default: 1 ,
-            $el: {},
-            $options: [
-              {
-                label: '开启',
-                value: 1
+            {
+              $type: 'select',
+              $id: 'status',
+              label: '状态',
+              $el: {
+                op: 'eq',
+                placeholder: '请选择状态'
               },
-              {
-                label: '禁用',
-                value: 0
-              }
-            ],
-          }
-        ]
+              $options: [
+                {
+                  label: '全部',
+                  value: ''
+                },
+                {
+                  label: '开启',
+                  value: '1'
+                },
+                {
+                  label: '禁用',
+                  value: '0'
+                }
+              ]
+            }
+          ],
+          //  弹窗表单, 用于新增与修改
+          form: [
+            {
+              $type: 'input',
+              $id: 'name',
+              label: '名称',
+              $el: {
+                placeholder: '请输入名称'
+              },
+              rules: [
+                {
+                  required: true,
+                  message: '请输入名称',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'code',
+              label: '代码',
+              $el: {
+                placeholder: '请输入代码'
+              },
+              rules: [
+                {
+                  required: true,
+                  message: '请输入代码',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'symbolLeft',
+              label: '左侧符',
+              $el: {
+                placeholder: '请输入左侧符'
+              },
+              rules: [
+                {
+                  required: true,
+                  message: '请输入左侧符',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'symbolRight',
+              label: '右侧符',
+              $el: {
+                placeholder: '请输入右侧符'
+              },
+              rules: [
+                {
+                  required: true,
+                  message: '请输入右侧符',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'decimalPoint',
+              label: '小数符',
+              $default: '.',
+              $el: {
+                placeholder: '请输入小数符'
+              },
+              rules: [
+                {
+                  max: 1,
+                  message: '长度不能超过1位',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'thousandsPoint',
+              label: '千分位符',
+              $default: ',',
+              $el: {
+                placeholder: '请输入千分位符'
+              },
+              rules: [
+                {
+                  max: 1,
+                  message: '长度不能超过1位',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'decimalPlaces',
+              label: '小数位数',
+              $default: 2,
+              $el: {
+                placeholder: '请输入小数位数',
+              },
+              rules: [
+                {
+                  whitespace: true,
+                  type: 'integer',
+                  transform(value) {
+                    if (value) {
+                      return Number(value);
+                    }
+                  },
+                  message: '小数位数必须为正整数',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'value',
+              label: '汇率值',
+              $default: '1',
+              $el: {
+                placeholder: '请输入汇率值'
+              },
+              rules: [
+                {
+                  whitespace: true,
+                  type: 'number',
+                  transform(value) {
+                    if (value) {
+                      return Number(value);
+                    }
+                  },
+                  message: '汇率值必须为数字类型',
+                  trigger: 'blur'
+                }
+              ]
+            },
+            {
+              $type: 'radio-group',
+              $id: 'status',
+              label: '状态',
+              $default: 1,
+              $el: {},
+              $options: [
+                {
+                  label: '开启',
+                  value: 1
+                },
+                {
+                  label: '禁用',
+                  value: 0
+                }
+              ],
+            }
+          ]
+        }
       }
     },
     computed: {},
