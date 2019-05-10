42f865089c5ef21d3c5a6313036aa70026bbc947
loveswimmingfish <33740290+loveswimmingfish@users.noreply.github.com>
2019-05-10 17:20:03 +0800

    菜单

---
+++ src/views/Menu1/index.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="app-container">
+    <div class="ph-card">
+      <ph-card-header :title="title" type="table">
+      </ph-card-header>
+      <div class="ph-card-body">
+        <ph-table
+          v-bind="tableConfig"
+        >
+        </ph-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {parseTime} from '@/utils'
+  import validRules from '@/components/validrules'
+
+  export default {
+    data() {
+      return {
+        title: '菜单列表',
+        tableConfig: {
+          url: '/menus',
+          relations: ["creator"],
+          tableAttrs: {
+            stripe: true,
+            border: true,
+            "row-class-name": this.statusClassName,
+            "highlight-current-row": true
+          },
+          columns: [
+            {type: 'selection'},
+            {prop: 'id', label: 'ID', sortable: 'custom', hidden: true},
+            {prop: 'level', label: '菜单级别', sortable: 'custom', 'min-width': 50, fixed: 'left'},
+            {prop: 'title', label: '名称', sortable: 'custom' ,'min-width': 80},
+            {prop: 'actionId', label: '操作标识', sortable: 'custom' ,'min-width': 100},
+            {prop: 'icon', label: '图标', sortable: 'custom' ,'min-width': 100},
+            {prop: 'parentId', label: '父级菜单ID', sortable: 'custom' ,'min-width': 60},
+            {prop: 'url', label: '菜单URL', sortable: 'custom' ,'min-width': 100},
+            {prop: 'sortNum', label: '排序值', sortable: 'custom' ,'min-width': 50},
+            {
+              prop: 'status',
+              label: '状态',
+              width: 80,
+              formatter: row => (row.status === 1 ? '启用' : '禁用')
+            },
+            {
+              prop: 'lastModified',
+              label: '修改时间',
+              width: 140,
+              formatter: row => {
+                return parseTime(row.lastModified, '{y}-{m}-{d} {h}:{i}');
+              }
+            }
+          ],
+          //搜索栏
+          searchForm: [
+             {
+              $type: 'select',
+              $id: 'level',
+              label: '菜单级别',
+              $el: {
+                op: 'eq',
+                placeholder: '请选择菜单级别'
+              },
+              $options: [
+                {
+                  label: '全部',
+                  value: ''
+                },
+                {
+                  label: '一级',
+                  value: '1'
+                },
+                {
+                  label: '二级',
+                  value: '2'
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'name',
+              label: '名称',
+              $el: {
+                op: 'bw',
+                placeholder: '请输入名称'
+              }
+            },
+             {
+              $type: 'input',
+              $id: 'actionId',
+              label: '操作标识',
+              $el: {
+                op: 'bw',
+                placeholder: '请输入操作标识'
+              }
+            },
+            {
+              $type: 'select',
+              $id: 'status',
+              label: '状态',
+              $el: {
+                op: 'eq',
+                placeholder: '请选择状态'
+              },
+              $options: [
+                {
+                  label: '全部',
+                  value: ''
+                },
+                {
+                  label: '开启',
+                  value: '1'
+                },
+                {
+                  label: '禁用',
+                  value: '0'
+                }
+              ]
+            }
+          ],
+          //添加或修改弹出栏
+          form: [
+             {
+              $type: 'select',
+              $id: 'level',
+              label: '菜单级别',
+              $default: 1,
+              $el: {
+              },
+              $options: [
+                {
+                  label: '一级',
+                  value: 1
+                },
+                {
+                  label: '二级',
+                  value: 2
+                }
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'title',
+              label: '名称',
+              $el: {
+                placeholder: '请输入名称'
+              },
+              rules: [
+                validRules.required
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'actionId',
+              label: '操作标识',
+              $el: {
+                placeholder: '请输入操作标识'
+              }
+            },
+            {
+              $type: 'input',
+              $id: 'icon',
+              label: '图标',
+              $el: {
+              }
+            },
+            {
+              $type: 'input',
+              $id: 'parentId',
+              label: '父级菜单ID',
+              $el: {
+              }
+            },
+            {
+              $type: 'input',
+              $id: 'url',
+              label: '菜单URL',
+              $el: {
+              },
+              rules: [
+                validRules.required
+              ]
+            },
+            {
+              $type: 'input',
+              $id: 'sortNum',
+              label: '排序值',
+              $el: {
+              },
+              rules: [
+                validRules.required
+              ]
+            },
+            {
+              $type: 'radio-group',
+              $id: 'status',
+              label: '状态',
+              $el: {},
+              $default: 1,
+              $options: [
+                {
+                  label: '开启',
+                  value: 1
+                },
+                {
+                  label: '禁用',
+                  value: 0
+                }
+              ]
+            }
+          ]
+        }
+      }
+    },
+    computed: {},
+    methods: {
+      statusClassName({row, rowIndex}) {
+        if (row.status && row.status !== 0) {
+          return '';
+        }
+        else {
+          return 'warning-row';
+        }
+      },
+    },
+    watch: {}
+  }
+</script>
+
+<style scoped>
+
+
+</style>
