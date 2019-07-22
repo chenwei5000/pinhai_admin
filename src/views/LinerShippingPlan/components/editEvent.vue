f7d80a1a206623663b710731cef24ebbfcc2dd49
tankai <tank@1ycloud.net>
2019-07-22 10:32:02 +0800

    合并代码

--- src/views/LinerShippingPlan/components/editEvent.vue
+++ src/views/LinerShippingPlan/components/editEvent.vue
@@ -1,18 +1,256 @@
 <template>
-    <div>
-        
+  <el-dialog title="创建计划" :visible.sync="formVisible" width="60%">
+    <fieldset class="panel-heading">
+      <legend class="panel-title">基本信息</legend>
+      <el-form
+        :model="plan"
+        :rules="rules"
+        ref="form"
+        label-width="100px"
+        class="demo-ruleForm"
+        :inline="true"
+      >
+        <el-form-item label="开船时间" prop="etdTime">
+          <el-date-picker v-model="plan.etdTime" type="datetime" placeholder="选择日期时间"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="编号">
+          <el-input placeholder v-model="plan.code" :disabled="true"></el-input>
+        </el-form-item>
+
+        <el-form-item label="运输方式" prop="type">
+          <el-select v-model="plan.type" placeholder="请选择" style="width: 100%;">
+            <el-option label="海运" value="海运"></el-option>
+            <el-option label="空运" value="空运"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="发货港口" prop="portOfLoading">
+          <el-select v-model="plan.portOfLoading" placeholder="请选择">
+            <el-option
+              v-for="(item, idx) in harbours"
+              :key="`${idx}-${item.value}`"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="发货仓库" prop="fromWarehouseId">
+          <el-select v-model="plan.fromWarehouseId" filterable placeholder="请选择">
+            <el-option
+              v-for="(item, idx) in fromWarehouses"
+              :key="`${idx}-${item.value}`"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="收货仓库" prop="toWarehouseId">
+          <el-select v-model="plan.toWarehouseId" filterable placeholder="请选择">
+            <el-option
+              v-for="(item, idx) in toWarehouses"
+              :key="`${idx}-${item.value}`"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="分类" prop="categoryId">
+          <el-select v-model="plan.categoryId" multiple filterable placeholder="请选择">
+            <el-option
+              v-for="(item , idx) in categorys"
+              :key="`${idx}-${item.value}`"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="负责人" prop="merchandiserId">
+          <el-select v-model="plan.merchandiserId" multiple filterable placeholder="请选择">
+            <el-option
+              v-for="(item, idx) in users"
+              :key="`${idx}-${item.value}`"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="是否打托：">
+          <el-radio v-model="plan.pallet" label="1" value="1">是</el-radio>
+          <el-radio v-model="plan.pallet" label="0" value="0">否</el-radio>
+        </el-form-item>
+
+        <el-form-item label="Oversize：" style="margin-left:150px;">
+          <el-radio v-model="plan.oversize" label="1" value="1">是</el-radio>
+          <el-radio v-model="plan.oversize" label="2" value="0">否</el-radio>
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 4}"
+            placeholder="请输入内容"
+            v-model="plan.detail"
+            style="width:500px"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </fieldset>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="formVisible = false">取 消</el-button>
+      <el-button type="primary" @click="createPlan">确 定</el-button>
     </div>
+  </el-dialog>
 </template>
 
-
-
 <script>
+import harbourModel from "../../../api/harbour";
+import categoryModel from "../../../api/category";
+import userModel from "../../../api/user";
+import planModel from "../../../api/linerShippingPlan";
+
+// loading 组件
+import { Loading } from "element-ui";
+
+let loadingInstance = null;
+
+const showLoading = () => {
+  loadingInstance = Loading.service({ fullscreen: true });
+};
+const closeLoding = () => {
+  if (loadingInstance) {
+    loadingInstance.close();
+  }
+};
+
 export default {
-    
-}
+  props: {},
+
+  data() {
+    return {
+      fromWarehouses: [],
+      toWarehouses: [],
+      harbours: [],
+      categorys: [],
+      users: [],
+
+      formVisible: false,
+
+      plan: {
+        etdTime: "",
+        code: "",
+        type: "",
+        portOfLoading: "",
+        fromWarehouseId: "",
+        toWarehouseId: "",
+        categoryId: [],
+        merchandiserId: [],
+        pallet: "",
+        oversize: "",
+        detail: ""
+      },
+      rules: {
+        etdTime: [
+          { required: true, message: "开船时间不能为空", trigger: "blur" }
+        ],
+        type: [
+          { required: true, message: "运输方式不能为空", trigger: "blur" }
+        ],
+        portOfLoading: [
+          { required: true, message: "发货港口不能为空", trigger: "blur" }
+        ],
+        fromWarehouseId: [
+          { required: true, message: "发货仓库不能为空", trigger: "blur" }
+        ],
+        toWarehouseId: [
+          { required: true, message: "收货仓库不能为空", trigger: "blur" }
+        ],
+        categoryId: [
+          { required: true, message: "分类不能为空", trigger: "blur" }
+        ],
+        merchandiserId: [
+          { required: true, message: "负责人不能为空", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    createPlan() {
+      this.$refs["planForm"].validate(valid => {
+        if (valid) {
+          this.plan.category = this.plan.categoryId.join(",");
+          this.plan.merchandiser = this.plan.merchandiserId.join(",");
+          showLoading();
+          this.global.axios
+            .post("/linerShippingPlans", this.plan)
+            .then(data => {
+              // 添加新的计划
+              if (data.status == 200) {
+                let event = data.data;
+                let title = planModel.generateEventTitle(event);
+                this.$emit("addCalendarEvent", event, title);
+                closeLoding();
+                this.formVisible = false;
+              }
+            });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  },
+  mounted() {
+    this.$on("openDialog", id => {
+      this.global.axios.get(`/linerShippingPlans/${id}`).then(data => {
+        if (data.status === 200) {
+          this.formVisible = true;
+          this.plan = data.data;
+          this.plan.etdTime = this.plan.formatEtdTime;
+          this.plan.categoryId = this.plan.categoryName.split(",");
+          this.plan.merchandiserId = this.plan.merchandiserName.split(",");
+        }
+      });
+      // 仓库信息
+      let warehousesUrl = `/warehouses?filters={"groupOp":"AND","rules":[{"field":"status","op":"eq","data":"1"}]}&sort=type asc,name`;
+      this.global.axios(warehousesUrl).then(data => {
+        if (data.status == 200) {
+          data.data.rows.forEach(warehouse => {
+            if (
+              warehouse.type == "工厂仓" ||
+              warehouse.type == "普通" ||
+              warehouse.type == "虚拟仓"
+            ) {
+              this.fromWarehouses.push({
+                value: warehouse.id,
+                label: warehouse.name
+              });
+            } else if (warehouse.type == "海外仓") {
+              this.toWarehouses.push({
+                value: warehouse.id,
+                label: warehouse.name
+              });
+            }
+          });
+        }
+      });
+
+      // 港口信息
+      this.harbours = harbourModel.getSelectNameOptions();
+
+      // 分类
+      this.categorys = categoryModel.getMineSelectNameOptions();
+
+      // 负责人
+      this.users = userModel.getSelectNameOptions();
+    });
+  }
+};
 </script>
 
 
 <style>
-
 </style>
