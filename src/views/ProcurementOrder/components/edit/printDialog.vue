1f2455d46c9f6c6adb380a68c226e9eb68861f6f
tankai <tank@1ycloud.net>
2019-08-30 12:56:27 +0800

    打印采购单

--- src/views/ProcurementOrder/components/edit/printDialog.vue
+++ src/views/ProcurementOrder/components/edit/printDialog.vue
@@ -19,6 +19,9 @@
           <el-form :rules="rules" :model="printObject" status-icon inline
                    ref="detailItem" label-position="right"
                    label-width="120px"
+                   inline-message
+                   v-if="initComplete"
+                   @submit.native.prevent
                    v-loading="loading"
           >
             <fieldset class="panel-heading">
@@ -37,7 +40,11 @@
                     <el-input v-model.trim="printObject.demanderName"
                               maxlength="100"
                               show-word-limit
-                              style="width: 200px" placeholder="请填写需求方公司名称" clearable></el-input>
+                              name="demanderName"
+                              @input="updateInput"
+                              style="width: 200px" placeholder="请填写需求方公司名称" clearable>
+
+                    </el-input>
                   </el-form-item>
                 </el-col>
 
@@ -45,7 +52,9 @@
                   <el-form-item label="公司地址" prop="demanderAddress">
                     <el-input v-model.trim="printObject.demanderAddress"
                               maxlength="200"
+                              name="demanderAddress"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写需求方公司地址" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -56,7 +65,9 @@
                   <el-form-item label="联系人" prop="demanderLinkMan">
                     <el-input v-model.trim="printObject.demanderLinkMan"
                               maxlength="20"
+                              name="demanderLinkMan"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 200px" placeholder="请填写需求方联系人" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -65,7 +76,9 @@
                   <el-form-item label="联系电话" prop="demanderLinkMethod">
                     <el-input v-model.trim="printObject.demanderLinkMethod"
                               maxlength="20"
+                              name="demanderLinkMethod"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写需求方联系电话" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -76,7 +89,9 @@
                   <el-form-item label="收货地址" prop="demanderDeliveryAddress">
                     <el-input v-model.trim="printObject.demanderDeliveryAddress"
                               maxlength="200"
+                              name="demanderDeliveryAddress"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写需求收货地址" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -98,7 +113,9 @@
                   <el-form-item label="公司名称" prop="supplierName">
                     <el-input v-model.trim="printObject.supplierName"
                               maxlength="100"
+                              name="supplierName"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 200px" placeholder="请填写供货方公司名称" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -107,7 +124,9 @@
                   <el-form-item label="公司地址" prop="supplierAddress">
                     <el-input v-model.trim="printObject.supplierAddress"
                               maxlength="200"
+                              name="supplierAddress"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写供货方公司地址" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -118,7 +137,9 @@
                   <el-form-item label="联系人" prop="supplierLinkMan">
                     <el-input v-model.trim="printObject.supplierLinkMan"
                               maxlength="20"
+                              name="supplierLinkMan"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 200px" placeholder="请填写供货方联系人" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -127,7 +148,9 @@
                   <el-form-item label="联系电话" prop="supplierLinkMethod">
                     <el-input v-model.trim="printObject.supplierLinkMethod"
                               maxlength="20"
+                              name="supplierLinkMethod"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写供货方联系电话" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -138,7 +161,9 @@
                   <el-form-item label="收款银行" prop="supplierBankName">
                     <el-input v-model.trim="printObject.supplierBankName"
                               maxlength="20"
+                              name="supplierBankName"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 200px" placeholder="请填写供货方收款银行" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -147,7 +172,9 @@
                   <el-form-item label="收款户名" prop="supplierBankAccountName">
                     <el-input v-model.trim="printObject.supplierBankAccountName"
                               maxlength="20"
+                              name="supplierBankAccountName"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写供货方收款户名" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -158,7 +185,9 @@
                   <el-form-item label="收款账号" prop="supplierBankAccount">
                     <el-input v-model.trim="printObject.supplierBankAccount"
                               maxlength="20"
+                              name="supplierBankAccount"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写供货方收款账号" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -180,7 +209,9 @@
                   <el-form-item label="采购品类" prop="categoryName">
                     <el-input v-model.trim="printObject.categoryName"
                               maxlength="20"
+                              name="categoryName"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 200px" placeholder="请填写采购品类" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -189,7 +220,9 @@
                   <el-form-item label="结算货币" prop="currency">
                     <el-input v-model.trim="printObject.currency"
                               maxlength="20"
+                              name="currency"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写结算货币" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -198,24 +231,32 @@
               <el-row>
                 <el-col :md="10">
                   <el-form-item label="交货时间" prop="deliveryTime">
-                    <el-input v-model.trim="printObject.deliveryTime"
-                              maxlength="20"
-                              show-word-limit
-                              style="width: 200px" placeholder="请填写交货时间" clearable></el-input>
+                    <el-date-picker
+                      v-model="printObject.deliveryTime"
+                      name="deliveryTime"
+                      type="date"
+                      size="small"
+                      format="yyyy年M月d日"
+                      placeholder="选择交货时间">
+                    </el-date-picker>
                   </el-form-item>
                 </el-col>
 
                 <el-col :md="14">
+                  <el-input type="hidden" style="display: none" v-model="printObject.effectiveDateStart"
+                            name="effectiveDateStart"></el-input>
+                  <el-input type="hidden" style="display: none" v-model="printObject.effectiveDateEnd"
+                            name="effectiveDateEnd"></el-input>
                   <el-form-item label="生效日期" prop="effectiveDate">
-
                     <el-date-picker
                       v-model="printObject.effectiveDate"
                       type="daterange"
+                      size="small"
+                      format="yyyy年M月d日"
                       range-separator="至"
                       start-placeholder="开始日期"
                       end-placeholder="结束日期">
                     </el-date-picker>
-
                   </el-form-item>
                 </el-col>
               </el-row>
@@ -225,7 +266,9 @@
                   <el-form-item label="签约地址" prop="signAddress">
                     <el-input v-model.trim="printObject.signAddress"
                               maxlength="200"
+                              name="signAddress"
                               show-word-limit
+                              @input="updateInput"
                               style="width: 300px" placeholder="请填写签约地址" clearable></el-input>
                   </el-form-item>
                 </el-col>
@@ -240,6 +283,7 @@
     </div>
 
     <div slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="onPrint" :loading="confirmLoading">生成合同</el-button>
       <el-button @click="closeDialog">关 闭</el-button>
     </div>
 
@@ -250,6 +294,8 @@
 <script>
 
   import {mapGetters} from 'vuex'
+  import {parseTime} from '@/utils'
+  import validRules from '@/components/validRules'
 
   export default {
     components: {},
@@ -270,7 +316,58 @@
         printObject: {},
         dialogVisible: false, //Dialog 是否开启
         loading: false,
-        rules: {},
+        initComplete: false,
+        confirmLoading: false,
+        rules: {
+          demanderName: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          demanderAddress: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          demanderLinkMan: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          demanderLinkMethod: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          supplierName: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          supplierAddress: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          supplierLinkMan: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          supplierLinkMethod: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          supplierBankName: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          supplierBankAccountName: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          supplierBankAccount: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          categoryName: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          currency: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          deliveryTime: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          effectiveDate: [
+            {required: true, message: '*', trigger: 'blur'}
+          ],
+          signAddress: [
+            {required: true, message: '*', trigger: 'blur'}
+          ]
+        },
       }
     },
 
@@ -283,6 +380,7 @@
     },
     methods: {
       initData() {
+        this.initComplete = false;
         this.printObject.demanderName = '深圳市品海电⼦商务有限公司';
         this.printObject.demanderAddress = '深圳市龙岗区坂田街道永香路江南大厦二楼212室';
         this.printObject.demanderDeliveryAddress = this.primary.warehouse.address;
@@ -309,9 +407,8 @@
         url += "&relations=" + JSON.stringify(relations);
 
         // 请求开始
-        this.loading = true
+        this.loading = true;
 
-        console.log(url);
         //获取数据
         this.global.axios
           .get(url)
@@ -328,9 +425,11 @@
             this.printObject.categoryName = cates.join(",");
 
             this.loading = false
+            this.initComplete = true;
           })
           .catch(err => {
             this.loading = false
+            this.initComplete = true;
           })
       },
 
@@ -338,7 +437,6 @@
       /* 开启弹出编辑框 需要传主键ID */
       openDialog(primary) {
         this.primary = primary;
-        console.log(this.primary);
         this.initData();
         this.dialogVisible = true;
       },
@@ -352,6 +450,29 @@
       onPaymentCBEvent(object) {
         this.$emit("paymentCBEvent", object);
       },
+      /* 打印合同 */
+      onPrint() {
+        this.$refs.detailItem.validate(valid => {
+          if (!valid) {
+            return false
+          }
+          // ajax获取合同数据
+          if (this.printObject.effectiveDate) {
+            this.printObject.effectiveDateStart = parseTime(this.printObject.effectiveDate[0], "{y}年{m}月{d}日");
+            this.printObject.effectiveDateEnd = parseTime(this.printObject.effectiveDate[1], "{y}年{m}月{d}日");
+          }
+          let url = `${this.global.generateUrl("/pdfs/purchaseContract")}/${this.primary.id}?accessToken=${this.$store.state.user.token}`;
+          let form = this.$refs.detailItem.$el;
+          console.log(form);
+          form.action = url;
+          form.method = "POST";
+          form.target = "_blank";
+          this.$refs.detailItem.$el.submit();
+        });
+      },
+      updateInput(val){
+        this.$forceUpdate();
+      }
     }
   }
 </script>
