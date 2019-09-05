b1e283f79d783f6ae74870eca2ebe015ead1325c
loveswimmingfish <33740290+loveswimmingfish@users.noreply.github.com>
2019-09-05 14:55:57 +0800

    调拨

--- src/views/WarehouseAllocation/components/smart/dialog.vue
+++ src/views/WarehouseAllocation/components/smart/dialog.vue
@@ -10,10 +10,12 @@
 
     <div class="ph-form">
       <!-- 编辑表单 TODO:-->
+      {{this.detailItem}}
       <el-form :rules="rules" :model="detailItem" status-icon inline
                ref="detailItem" label-position="right"
                label-width="120px"
                v-loading="loading"
+               :data="detailItem"
       >
         <el-row>
           <el-col :md="10">
@@ -21,16 +23,12 @@
               <el-input v-model.trim="detailItem.skuCode"
                         style="width: 200px" placeholder="请填写SKU" clearable>
               </el-input>
-
-              <el-tooltip class="item" effect="light" content="输入产品SKU编码" placement="right">
-                <i class="el-icon-question">&nbsp;</i>
-              </el-tooltip>
             </el-form-item>
           </el-col>
 
           <el-col :md="14">
             <el-form-item label="产品名" prop="productName">
-              <span style="font-size: 12px">{{detailItem.productName}}</span>
+              <span style="font-size: 12px" v-model="detailItem.productName">{{detailItem.productName}}</span>
             </el-form-item>
           </el-col>
 
@@ -39,7 +37,6 @@
         <el-row>
           <el-col :md="10">
             <el-form-item label="箱规" prop="cartonSpecId">
-
               <el-select filterable v-model="detailItem.cartonSpecId" placeholder="外箱包装材料规格,可筛选"
                          style="width: 200px">
                 <el-option
@@ -86,18 +83,12 @@
                                :step="1"
                                :max="1000000" label="请填写应发箱数">
               </el-input-number>
-
-
-              <el-tooltip class="item" effect="light" content="应发箱数,支持3位小树。" placement="right">
-                <i class="el-icon-question">&nbsp;</i>
-              </el-tooltip>
-
             </el-form-item>
           </el-col>
 
           <el-col :md="14">
             <el-form-item :label="shippedQtyTitle" prop="shippedQty">
-              <span style="font-size: 12px">{{shippedQty}}</span>
+              <span style="font-size: 12px" v-model="detailItem.shippedQty">{{shippedQty}}</span>
             </el-form-item>
           </el-col>
         </el-row>
@@ -106,23 +97,13 @@
           <el-col :md="24">
             <el-form-item label="备注" prop="priorityNote">
               <el-col :span="22">
-                <el-input type="textarea" v-model="detailItem.shippedNote"
+                <el-input type="textarea" v-model="detailItem.remark"
                           maxlength="500"
                           show-word-limit
                           rows="3"
                           cols="80"
                 ></el-input>
               </el-col>
-
-              <el-col :span="2">
-
-                <el-tooltip class="item" effect="light" placement="right">
-                  <div slot="content">发货备注</div>
-                  <i class="el-icon-question">&nbsp;</i>
-                </el-tooltip>
-
-              </el-col>
-
             </el-form-item>
           </el-col>
         </el-row>
@@ -154,9 +135,6 @@
       hasAdd() {
         return (this.detailItemId == null);
       },
-      procurementBoxQtyTitle() {
-        return `采购数量(${this.unit})`;
-      },
       shippedQtyTitle() {
         return `应发${this.unit == '箱' ? '件' : this.unit}数`;
       },
@@ -183,6 +161,7 @@
             shippedCartonQty: null,
             skuCode: null,
             shippedQty: null,
+            cartonSpecCode: null,
         },
         cartonspecSelectOptions: [],
         unit: "箱",
@@ -212,26 +191,17 @@
       this.$nextTick(() => {
       });
     },
-
+    updated(){
+    },
     methods: {
       /********************* 基础方法  *****************************/
       //初始化加载数据 TODO:根据实际情况调整
       initData() {
         //获取数据
         // 箱规
-        this.cartonspecSelectOptions = cartonspecModel.getSelectOptions();
-          // 设置添加默认值
-          this.detailItem = {
-            skuCode: '',
-            cartonSpecId: null,
-            numberOfCarton: null,
-            shippedCartonQty: 1,
-            warehouseAllocationId: null,
-          }
+   
       },
 
-
-      
       // 计算发货件数
       calShippedQty() {
         if (this.detailItem.shippedCartonQty && this.detailItem.numberOfCarton) {
@@ -243,17 +213,19 @@
       },
 
       /* 开启弹出编辑框 需要传明细ID */
-      openDialog() {
+      openDialog(row) {
+        this.cartonspecSelectOptions = cartonspecModel.getSelectOptions();
         this.dialogVisible = true;
+        if(row != null){
+           this.detailItem =  JSON.parse(JSON.stringify(row));
+        }
       },
 
       closeDialog() {
         this.dialogVisible = false;
         this.loading = false;
         this.confirmLoading = false;
-        this.detailItemId = null;
         this.detailItem = {};
-        this.order = null;
         this.unit = "箱";
       },
 
@@ -264,7 +236,7 @@
         else {
           this.loading = true;
           this.confirmLoading = true;
-          let url = `/products/sku/${this.detailItem.skuCode}`;
+          let url = `/products/sku/${this.detailItem.skuCode}`+ "?relations=" + JSON.stringify(["cartonSpec", "category"]);;
           this.global.axios
             .get(url)
             .then(resp => {
@@ -273,7 +245,7 @@
 
               this.detailItem.cartonSpecId = data.cartonSpecId + '';
               this.detailItem.numberOfCarton = data.numberOfCarton;
-              console.log(data);
+              this.detailItem.cartonSpecCode = data.cartonSpecCode;
               // 转字段
               this.detailItem.productName = data.name;
               if (data.cartonSpecId == -3) { //原料采购
@@ -294,7 +266,10 @@
           if (!valid) {
             return false
           }
-          console.log("保存调拨明细！", this.detailItem)
+         this.detailItem.shippedQty = this.calShippedQty();
+         console.log("即将保存的对象 ", this.detailItem);
+         this.$emit("modifyCBEvent", this.detailItem);
+         this.closeDialog();
         })
       }
     }
--- src/views/WarehouseAllocation/components/smart/table.vue
+++ src/views/WarehouseAllocation/components/smart/table.vue
@@ -25,59 +25,16 @@
       :data="tableData"
       v-loading="loading"
       show-summary
-      :default-sort="{prop: 'product.skuCode', order: 'ascending'}"
-      id="table"
     >
-      <el-table-column prop="sortNum" label="序号" width="50"></el-table-column>
-
-      <el-table-column prop="skuCode" label="SKU" sortable min-width="150">
-        <template slot-scope="scope">
-          <el-popover placement="top-start" width="200" trigger="hover"
-                      v-if="scope.row.product.skuCode && scope.row.product.skuCode.length > 30">
-            <div v-html="scope.row.product.skuCode"></div>
-            <span slot="reference">{{
-              scope.row.product.skuCode ? scope.row.product.skuCode.length > 22 ? scope.row.product.skuCode.substr(0,20)+'..' : scope.row.product.skuCode : ''
-              }}</span>
-          </el-popover>
-          <span v-else>
-            {{ scope.row.skuCode }}
-          </span>
-        </template>
+      <el-table-column label="序号" width="50" type=index></el-table-column>
+      <el-table-column prop="skuCode" label="SKU" sortable min-width="150"></el-table-column>
+      <el-table-column prop="productName" label="名称" width="200"></el-table-column>
+      <el-table-column prop="cartonSpecCode" label="箱规" min-width="120">
       </el-table-column>
-
-      <el-table-column prop="productName" label="名称" width="200">
-        <template slot-scope="scope">
-          <el-popover placement="top-start" width="200" trigger="hover"
-                      v-if="scope.row.product.name && scope.row.product.name.length > 17">
-            <div v-html="scope.row.product.name"></div>
-            <span slot="reference">{{
-              scope.row.product.name ? scope.row.product.name.length > 17 ? scope.row.product.name.substr(0,15)+'..' : scope.row.product.name : ''
-              }}</span>
-          </el-popover>
-          <span v-else>
-            {{ scope.row.product.name }}
-            </span>
-        </template>
-      </el-table-column>
-
-      <el-table-column prop="cartonSpecId" label="箱规" min-width="120"></el-table-column>
-
       <el-table-column prop="numberOfCarton" label="装箱数" min-width="80"></el-table-column>
       <el-table-column prop="shippedCartonQty" label="调拨箱数" min-width="110"></el-table-column>
       <el-table-column prop="shippedQty" sortable :label="shippedQtyTitle" min-width="110"></el-table-column>
-
-      <el-table-column prop="remark" label="备注" width="130">
-        <template slot-scope="scope">
-          <el-popover placement="top-start" title="备注" width="250" trigger="hover"
-                      v-if="scope.row.shippedNote && scope.row.shippedNote.length > 10">
-            <div v-html="scope.row.formatShippedNote"></div>
-            <span slot="reference">{{ scope.row.shippedNote ? scope.row.shippedNote.substr(0,8)+'..' : '' }}</span>
-          </el-popover>
-          <span v-else>
-            {{ scope.row.shippedNote }}
-          </span>
-        </template>
-      </el-table-column>
+      <el-table-column prop="remark" label="备注" width="130"></el-table-column>
 
       <!--默认操作列-->
       <el-table-column label="操作" v-if="hasOperation"
@@ -85,13 +42,13 @@
                        width="120" fixed="right">
         <template slot-scope="scope">
 
-          <el-button v-if="hasEdit" size="small" icon="el-icon-edit" circle
-                     @click="onDefaultEdit(scope.row)" type="primary" id="ph-table-edit">
+          <el-button size="small" icon="el-icon-edit" circle
+                     @click="onDefaultEdit(scope.row)" type="primary" id="ph-table-edit" >
           </el-button>
 
-          <el-button v-if="hasDelete" type="danger" size="mini"
+          <el-button type="danger" size="mini"
                      id="ph-table-del" icon="el-icon-delete" circle
-                     @click="onDefaultDelete(scope.row)">
+                     @click="onDefaultDelete(scope.$index)">
 
           </el-button>
         </template>
@@ -118,10 +75,6 @@
       itemDialog
     },
     props: {
-      primary: {
-        type: [Object],
-        default: null
-      }
     },
     computed: {
       ...mapGetters([
@@ -139,10 +92,8 @@
         // 选择项
         // 表格最大高度
         tableMaxHeight: this.device !== 'mobile' ? 500 : 40000000,
-
         // 点击按钮之后，按钮锁定不可在点
         confirmLoading: false,
-
         //操作按钮控制
         hasOperation: true,
         hasAdd: true,
@@ -150,14 +101,6 @@
         hasDelete: true,
 
         tableData: [
-          {
-            cartonSpecId: 1,
-            numberOfCarton: 2,
-            productName: 3,
-            shippedCartonQty: 4,
-            skuCode: 5,
-            shippedQty: 6,
-        },
         ],  // 前端表格显示的数据，本地搜索用
         // 表格加载效果
         loading: false,
@@ -192,16 +135,18 @@
       /********************* 操作按钮相关方法  ***************************/
       /* 行修改功能 */
       onDefaultEdit(row) {
-        this.$refs.itemDialog.openDialog(row.id);
+        console.log("传给子类的编进", row)
+        this.$refs.itemDialog.openDialog(row);
       },
 
       /* 行删除功能 */
-      onDefaultDelete(row) {
+      onDefaultDelete(index) {
         this.$confirm('确认删除吗', '提示', {
           type: 'warning',
           beforeClose: (action, instance, done) => {
             if (action == 'confirm') {
-            
+                this.tableData.splice(index, 1);
+                done();
             } else done()
           }
         }).catch(er => {
@@ -211,7 +156,17 @@
 
       /* 子组件编辑完成后相应事件 */
       modifyCBEvent(object) {
-        // 继续向父组件抛出事件 修改成功刷新列表
+          let flag = true; //是否存在此SKU的产品
+          this.tableData.forEach((item, index, arr) => {
+            if (item.skuCode === object.skuCode) {
+              arr[index] = object;
+              flag = false;
+            }
+          });
+          console.log(this.tableData);
+          if(flag){
+            this.tableData.push(object);
+          }
       },
 
       /********************* 工具条按钮  ***************************/
