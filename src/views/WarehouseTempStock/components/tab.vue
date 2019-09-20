a94270b550815981397282e11d3b95c97cf21536
loveswimmingfish <33740290+loveswimmingfish@users.noreply.github.com>
2019-09-20 18:28:45 +0800

    something

---
+++ src/views/WarehouseTempStock/components/tab.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <infoTable ref="infoTable" :type="type" :defaultFilters="filters"></infoTable>
+
+</template>
+
+<script>
+  import infoTable from './table'
+
+  export default {
+    components: {
+      infoTable
+    },
+    props: {
+      type: {
+        type: String,
+        default: 'valid'
+      }
+    },
+    computed: {
+      // TODO: 设置默认的搜索条件
+      filters() {
+        if (this.type === 'all') {
+          return {
+            field: 'type',
+            op: 'in',
+            data: "原料仓"
+          }
+        }
+      }
+    },
+    methods: {
+      onRefreshTable() {
+        this.$refs.infoTable.onRefreshTable();
+      }
+    }
+  }
+</script>
+
---
+++ src/views/WarehouseTempStock/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="app-container">
+
+    <div class="ph-card">
+      <div class="ph-card-body">
+
+        <el-tabs v-model="activeStatus" type="border-card" @tab-click="handleTabClick">
+          <el-tab-pane name="all" lazy>
+            <span slot="label">
+              <i class="el-icon-s-order"></i> 全部
+            </span>
+            <keep-alive>
+              <phTab type="all"/>
+            </keep-alive>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import phTab from './components/tab'
+
+  const actionFlag = 's=';
+
+  export default {
+
+    components: {
+      phTab,
+    },
+
+    data() {
+      return {
+        // TODO 页面标题
+        title: '原料仓临时库存',
+        // TODO 默认Tab激活状态
+        activeStatus: location.href.indexOf(actionFlag) > -1
+          ? (this.$route.query.s !== null ? this.$route.query.s : 'all')
+          : 'all',
+      }
+    },
+
+    computed: {},
+
+    // 各种相关方法定义
+    methods: {
+      /* 点击Tag相应事件 */
+      // TODO: 通过URL记录点击Tab，方便刷新后不会切换视图
+      handleTabClick(tab, event) {
+        const queryFlag = '?s=';
+        const queryPath = '/m3/WarehouseTempStock_index';
+        let newUrl = location.origin + "/#" + queryPath + queryFlag + this.activeStatus;
+        history.pushState(history.state, 'ph-table search', newUrl);
+      },
+    },
+
+  }
+</script>
+
+<style type="text/less" lang="scss" scoped>
+
+</style>
