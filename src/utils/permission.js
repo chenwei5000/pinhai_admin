cd9e025fd1ac82ef79a9ee402cd9d1f8a97ccc9e
tankai <tank@1ycloud.net>
2019-10-08 11:07:07 +0800

    基础表格权限控制

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -5,21 +5,14 @@ import store from '@/store'
  * @returns {Boolean}
  * @example see @/views/permission/directive.vue
  */
-export default function checkPermission(value) {
-  if (value && value instanceof Array && value.length > 0) {
-    const roles = store.getters && store.getters.roles
-    const permissionRoles = value
-
-    const hasPermission = roles.some(role => {
-      return permissionRoles.includes(role)
-    })
-
-    if (!hasPermission) {
-      return false
+export function checkPermission(value) {
+  if (value && value.length > 0) {
+    const roles = store.getters && store.getters.rolePower;
+    if (roles) {
+      return roles.hasOwnProperty(value);
     }
-    return true
+    return false;
   } else {
-    console.error(`need roles! Like v-permission="['admin','editor']"`)
     return false
   }
 }
