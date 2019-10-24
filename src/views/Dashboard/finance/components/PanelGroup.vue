c2fb58f6139efa870e68f99622bdc7f88169394a
zsy <1196776120@qq.com>
2019-10-24 16:55:28 +0800

    财务Dashboard

---
+++ src/views/Dashboard/finance/components/PanelGroup.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <el-card class="box-card" style="margin-top:10px;">
+
+    <div slot="header">
+      <svg-icon icon-class="business"/>
+      <span style="margin-left:10px;">财务</span>
+    </div>
+
+    <el-row :gutter="40" class="panel-group">
+
+      <el-col :xs="8" :sm="8" :lg="4" class="card-panel-col">
+        <logistic-payment :mine="mine"></logistic-payment>
+      </el-col>
+
+      <el-col :xs="8" :sm="8" :lg="4" class="card-panel-col">
+        <payment :mine="mine"></payment>
+      </el-col>
+
+      <el-col :xs="8" :sm="8" :lg="4" class="card-panel-col">
+        <pre-payment :mine="mine"></pre-payment>
+      </el-col>
+
+    </el-row>
+
+  </el-card>
+</template>
+
+<script>
+import LogisticPayment from "../panel/logisticPayment"
+import Payment from "../panel/payment"
+import PrePayment from "../panel/prePayment"
+
+  export default {
+    props: {
+      mine: {
+        type: Object,
+      }
+    },
+    components: {
+      Payment,
+      LogisticPayment,
+      PrePayment
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .panel-group {
+    margin-top: 10px;
+
+    .card-panel-col {
+      margin-bottom: 15px;
+      align-content: center;
+      text-align: center;
+    }
+
+  }
+
+</style>
--- src/views/Dashboard/stockManager/index.vue
+++ src/views/Dashboard/stockManager/index.vue
@@ -8,16 +8,6 @@
         <operate-group @handleSetLineChartData="handleSetLineChartDat
         a" :mine="mine"/>
 
-        <el-card class="box-card" style="margin-top:10px;">
-          <div slot="header" class="clearfix">
-            <svg-icon icon-class="sales-statistics"/>
-            <span style="margin-left:10px;">销售情况</span>
-          </div>
-          <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-            <line-chart :chart-data="lineChartData"/>
-          </el-row>
-        </el-card>
-
       </el-col>
       <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}"
               style="margin-bottom:30px;">
@@ -31,55 +21,13 @@
 <script>
   import OperateGroup from './components/OperateGroup'
   import PanelGroup from './components/PanelGroup'
-  import LineChart from './components/LineChart'
   import TodoList from './components/TodoList'
-
-  const lineChartData = {
-    newVisitis: {
-      expectedData: [100, 120, 161, 134, 105, 160, 165]
-    },
-    messages: {
-      expectedData: [200, 192, 120, 144, 160, 130, 140],
-      actualData: [180, 160, 151, 106, 145, 150, 130]
-    },
-    purchases: {
-      expectedData: [80, 100, 121, 104, 105, 90, 100],
-      actualData: [120, 90, 100, 138, 142, 130, 130]
-    },
-    shoppings: {
-      expectedData: [130, 140, 141, 142, 145, 150, 160],
-      actualData: [120, 82, 91, 154, 162, 140, 130]
-    }
-  }
-
-  const merchantOptions = [
-    {
-      label: 'Rose Home Fashion',
-      value: '8a23287966dc9acb0166dca2574c0000'
-    }
-  ]
-
-  const weekSelectOptions = [
-    {
-      label: '19周',
-      value: '19'
-    },
-    {
-      label: '20周',
-      value: '20'
-    },
-    {
-      label: '21周',
-      value: '21'
-    }
-  ]
 
   export default {
     name: 'DashboardAdmin',
     components: {
       PanelGroup,
       OperateGroup,
-      LineChart,
       TodoList
     },
     computed: {
@@ -90,17 +38,14 @@
     },
     data() {
       return {
-        lineChartData: lineChartData.newVisitis
       }
     },
     created() {
     },
     methods: {
       initData(){
-
       },
       handleSetLineChartData(type) {
-        this.lineChartData = lineChartData[type]
       }
     }
   }
