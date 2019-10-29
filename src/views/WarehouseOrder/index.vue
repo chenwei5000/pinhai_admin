fe6253a551f81c27e9d74c93749e3e73fb66840c
tankai <tank@1ycloud.net>
2019-10-29 18:49:17 +0800

    出入库记录增加供货商

--- src/views/WarehouseOrder/index.vue
+++ src/views/WarehouseOrder/index.vue
@@ -16,21 +16,25 @@
   import warehouseModel from '../../api/warehouse'
   import {checkPermission} from "../../utils/permission";
   import phEnumModel from '../../api/phEnum'
+  import supplierModel from "../../api/supplier";
 
   export default {
     data() {
       return {
         title: '出入库记录',
         tableConfig: {
+          exportFileName: '出入库记录',
+          noExportProps: ['结算状态','ID'],
+          paginationSize: 50,
           //权限控制
           hasNew: checkPermission('WarehouseOrderResource_create'),
           hasEdit: checkPermission('WarehouseOrderResource_update'),
           hasDelete: checkPermission('WarehouseOrderResource_remove'),
           // hasView: checkPermission('WarehouseOrderResource_get'),
-          hasExport: checkPermission('WarehouseOrderResource_export'),
+          hasExport: true, //checkPermission('WarehouseOrderResource_export'),
 
           url: '/warehouseOrders',
-          relations: ["product", "warehouse", "cartonSpec", "storageLocation", "currency"],
+          relations: ["product", "warehouse", "supplier", "cartonSpec", "storageLocation", "currency"],
           tableAttrs: {
             "row-class-name": this.statusClassName
           },
@@ -40,24 +44,27 @@
           'default-sort' : "{prop: 'createTime', order: 'descending'}",
           //列表
           columns: [
-            phColumns.id,
+            {prop: 'formatCreateTime', label: '创建时间', 'min-width': 140, fixed: 'left'},
             {prop: 'warehouse.name', label: '收货仓库', 'min-width': 90},
             {prop: 'code', label: '批次码', 'min-width': 130},
             {prop: 'allocationCode', label: '调拨单', 'min-width': 130},
             {prop: 'typeName', label: '类型', 'min-width': 100},
             /*{prop: 'storageLocation.code', label: '存放货位', 'min-width': 100},*/
+            {prop: 'supplier.name', label: '供货商', 'min-width': 100},
             {prop: 'product.skuCode', label: 'SKU编码', 'min-width': 150},
             {prop: 'product.name', label: '产品名', 'min-width': 150},
             {prop: 'numberOfCarton', label: '装箱数', 'min-width': 90},
-            {prop: 'cartonQty', label: '箱数', 'min-width': 90},
-            {prop: 'qty', label: '件数', 'min-width': 90},
-            {prop: 'usedQty', label: '消耗件数', 'min-width': 90},
 
             {prop: 'currency.name', label: '结算货币', 'min-width': 100, hidden: !checkPermission('PurchasePriceVisible')},
             phColumns.price,
             phColumns.amount,
             phColumns.jobStatus,
-            phColumns.createTime
+            phColumns.id,
+
+            {prop: 'cartonQty', label: '箱数', 'min-width': 90, fixed: 'right'},
+            {prop: 'qty', label: '件数', 'min-width': 90, fixed: 'right'},
+            {prop: 'usedQty', label: '消耗件数', 'min-width': 90, fixed: 'right'},
+
           ],
           // 搜索
           searchForm: [
@@ -120,6 +127,17 @@
                 'range-separator': '-',
                 'start-placeholder': '开始日期',
                 'end-placeholder': '结束日期'
+              }
+            },
+            {
+              $type: 'select',
+              $id: 'supplierId',
+              label: '供货商',
+              $options: supplierModel.getSelectOptions(),
+              $el: {
+                op: 'eq',
+                size: "mini",
+                placeholder: '请选择类型'
               }
             },
             {
