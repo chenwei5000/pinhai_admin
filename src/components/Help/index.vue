e28085b41e1042078718a58a5fde86cad6b152a3
tankai <tank@1ycloud.net>
2019-10-31 19:30:46 +0800

    帮助中心

--- src/components/Help/index.vue
+++ src/components/Help/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a  target="_blank" href="http://projects.kuihuajia.com/projects/erp/wiki/%E6%93%8D%E4%BD%9C%E6%89%8B%E5%86%8C" >
+    <a target="_blank" href="/static/help/index.html" >
       <svg-icon icon-class="help" />
     </a>
   </div>
