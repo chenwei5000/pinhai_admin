0b37c485a4151580faeec8e9af6216d7123b2734
tankai <tank@1ycloud.net>
2020-03-07 18:53:15 +0800

    fixbug 智能交货第二次交货提示无对应交货计划

---
+++ src/views/WarehouseStock/transportationStocks.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="app-container">
+    <div class="ph-card">
+      <div class="ph-card-body">
+
+        <div class="ph-form" style="padding: 10px 15px 0 15px;">
+          <!--搜索-->
+          <el-form :inline="true"
+                   :model="param"
+                   ref="searchForm"
+                   id="filter-form"
+                   inline-message
+                   :rules="rules"
+                   @submit.native.prevent>
+
+            <el-form-item label="分类" prop="category">
+              <el-select v-model="param.category" size="mini" style="width: 150px"
+                         multiple filterable placeholder="请选择分类" @change="onChange">
+                <el-option
+                  v-for="item in categories"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+
+            <el-form-item label="国内成品库存" prop="warehouse">
+              <el-select v-model="param.warehouse" size="mini" style="width: 150px" filterable multiple placeholder="请选择仓库">
+                <el-option
+                  v-for="item in warehouses"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+
+            <el-form-item>
+              <el-button native-type="submit" type="primary" @click="search" size="mini">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+
+
+        <ph-table
+          ref="table1"
+          v-bind="tableConfig"
+        >
+        </ph-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import categoryModel from '../../api/category'
+  import {checkPermission} from "../../utils/permission";
+  import validRules from "../../components/validRules";
+
+  export default {
+    data() {
+      return {
+        title: '成品安全库存-单位(件)',
+        param: {
+          category: '',
+          warehouse: '',
+        },
+        categories: [],
+        warehouses: [],
+
+        // 字段验证规则 TODO:
+        rules: {
+          category: [
+            validRules.required
+          ],
+          warehouse: [
+            validRules.required
+          ]
+        },
+
+        tableConfig: {
+          hasExport: true,
+          hasOperation: false,
+          hasPagination: false,
+          hasDelete: false,
+          hasEdit: false,
+          hasAdd: false,
+          exportFileName: '成品安全库存',
+          subHeight: 35,
+
+          url: null,
+          relations: ["safetyStocks"],
+          countUrl: "",
+          tableAttrs: {
+            "row-class-name": this.statusClassName,
+            "default-sort": {prop: 'safetyStocks.stockGapQty1', order: 'descending'},
+          },
+          //列表
+          columns: [
+            {prop: 'skuCode', label: 'SKU编码', 'min-width': 150},
+            {prop: 'productName', label: '产品名', 'min-width': 150},
+            {prop: 'categoryName', label: '分类', 'min-width': 80},
+            {prop: 'unit', label: '单位', 'min-width': 50, formatter: row => ('件')},
+          ],
+        }
+      }
+    },
+    computed: {},
+
+    mounted() {
+      this.initData();
+    },
+
+    methods: {
+      initData() {
+        this.categories = categoryModel.getMineSelectProdcutOptions();
+
+      },
+
+      resetSearch() {
+        this.param.category = null;
+        this.param.warehouse = null;
+      },
+
+      onChange() {
+        let cateId = this.param.category;
+        if (cateId != null) {
+          this.loading = true;
+          let url = "/warehouses/category";
+          url += "?cateId=" + cateId.join(",");
+          this.global.axios.get(url)
+            .then(resp => {
+              let res = resp.data || [];
+              this.warehouses = [];
+              res.forEach(r => {
+                this.warehouses.push({
+                  label: r.name,
+                  value: r.id + ''
+                });
+              });
+              this.loading = false;
+            })
+            .catch(err => {
+              this.loading = false;
+            });
+        }
+      },
+
+      statusClassName({row}) {
+        if (row && row.safetyStocks && (row.safetyStocks.stockGapQty3 > 0)) {
+          return 'danger-row';
+        }
+        else {
+          return '';
+        }
+      },
+
+      search() {
+        this.$refs.searchForm.validate(valid => {
+          if (valid) {
+            this.tableConfig.url = "/stocks/safetyStocks?category=";
+            if (this.param.category != null) {
+              this.tableConfig.url += this.param.category.join(",");
+            }
+            this.tableConfig.url += "&warehouse=";
+            if (this.param.warehouse != null) {
+              this.tableConfig.url += this.param.warehouse.join(",");
+            }
+            this.$refs.table1.getList();
+          }
+        })
+
+      }
+    },
+    watch: {}
+  }
+</script>
+
+<style type="text/less" lang="scss" scoped>
+
+  .ph-table {
+    padding: 0 15px !important;
+
+  }
+</style>
