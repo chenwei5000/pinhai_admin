6ba3ea26adbb7cfecbb77cef363b4f49940f32ac
zsy <1196776120@qq.com>
2020-04-01 15:40:06 +0800

    报关报表页面

--- src/views/DeclareForm/index.vue
+++ src/views/DeclareForm/index.vue
@@ -1,99 +1,198 @@
 <template>
-  <div class="app-container">
-    <div class="ph-card">
-      <div class="ph-card-body">
-        111
+  <el-card >
+    <div class="form-class">
+      <el-form :inline="true" inline-message>
+        <el-form-item label="请求地址" size="mini">
+          <el-input
+            placeholder="请输入请求地址"
+            size="mini"
+            v-model="searchParam.skuCode"
+            @keyup.enter.native="getTableData"
+            clearable
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="供货商" size="mini">
+          <el-select
+            size="mini"
+            filterable
+            v-model="searchParam.supplierId.value"
+            style="width: 120px"
+            placeholder="请选择供货商"
+          >
+            <el-option
+              v-for="(item,idx) in supplierSelectOptions"
+              :label="item.label"
+              :value="item.value"
+              :key="idx"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item size="mini">
+          <el-button size="mini" @click="getTableData()" type="primary">查询</el-button>
+          <el-button size="mini" @click="reset" type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+      <div>
+        <el-table
+          :data="tableData"
+          border
+          stripe
+          v-loading="loading"
+          element-loading-text="页面正在玩命加载中..."
+          element-loading-spinner="el-icon-loading"
+          height="500"
+        >
+          <el-table-column
+            prop="skuCode"
+            label="SKU编码"
+            min-width="100px"
+            width="300"
+            align="center"
+          ></el-table-column>
+          <el-table-column prop="supplierId" label="供货商ID" v-if="false"></el-table-column>
+          <el-table-column prop="supplierName" label="供货商" width="150" align="center"></el-table-column>
+          <el-table-column prop="price" label="平均价" align="center"></el-table-column>
+          <el-table-column prop="cartonQty" label="应发箱数" width="100%" align="center"></el-table-column>
+          <el-table-column prop="qty" label="应发件数" align="center"></el-table-column>
+          <el-table-column prop="sumCartonSpecWeight" label="应发毛重(Kg)" align="center"></el-table-column>
+          <el-table-column prop="cartonSpecVolume" label="应发体积(m³)" align="center"></el-table-column>
+        </el-table>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page.sync="currentPage"
+          :page-sizes="[10, 20, 50, 100]"
+          :page-size="pageSize"
+          :total="total"
+          background
+          style="text-align: right; padding: 10px 0"
+          layout="total, sizes, prev, pager, next, jumper"
+        >
+          <el-button icon="el-icon-refresh" @click="onRefreshTable" class="btn-prev" circle></el-button>
+        </el-pagination>
       </div>
     </div>
-  </div>
+  </el-card>
 </template>
 
 <script>
-  import validRules from '../../components/validRules'
-  import phColumns from '../../components/phColumns'
-  import phSearchItems from '../../components/phSearchItems'
-  import phFromItems from '../../components/phFromItems'
-  import {checkPermission} from "../../utils/permission";
+import supplierModel from "@/api/supplier";
 
-  export default {
-    data() {
-      return {
-        tableConfig: {
-          url: '/declareConfigs',
-          relations: ["creator"],
-          tableAttrs: {
-            "row-class-name": this.statusClassName
-          },
-          columns: [
-            {width: 30,type: checkPermission('DeclareConfigResource_remove') ? 'selection' : '', hidden: !checkPermission('DeclareConfigResource_remove')},
-            {prop: 'id', label: '报关码', "min-width": 100},
-            {prop: 'name', label: '申报品名(中)', "min-width": 200},
-            {prop: 'ename', label: '申报品名(英)', "min-width": 200},
-            {prop: 'unit', label: '报关单位', "min-width": 120},
-            {prop: 'tax', label: '税率', "min-width": 100},
-            {prop: 'refundTax', label: '退税率', "min-width": 100},
-            {prop: 'note', label: '报关备注', "min-width": 100},
-            phColumns.creator,
-            phColumns.status,
-            phColumns.lastModified
-          ],
+export default {
+  data() {
+    return {
+      searchParam: {
+        skuCode: "",
+        supplierId: { value: null, op: "in", id: "supplierId" }
+      },
+      tableData: [],
+      supplierSelectOptions: [],
+      currentPage: 1,
+      pageSize: 20,
+      total: 0,
+      loading: false
+    };
+  },
+  computed: {},
+  created() {
+    this.getTableData();
+  },
+  mounted() {},
 
-          // 搜索区块定义
-          searchForm: [
-            {
-              $type: 'input',
-              $id: 'id',
-              label: '报关码',
-              $el: {
-                op: 'in',
-                size: "mini",
-                placeholder: '请输入报关码',
-                clearable: true,
-              }
-            },
-            {
-              $type: 'input',
-              $id: 'name',
-              label: '申报品名(中)',
-              $el: {
-                op: 'bw',
-                placeholder: '请输入申报品名(中)',
-                size:"mini",
-                clearable: true,
-                maxlength: "40",
-                c: true,
-              }
-            },
-            {
-              $type: 'input',
-              $id: 'ename',
-              label: '申报品名(英)',
-              $el: {
-                op: 'bw',
-                placeholder: '请输入申报品名(英)',
-                size:"mini",
-                style: 'height:25px',
-                clearable: true,
-                maxlength: "40",
-                c: true,
-              }
-            },
-          ],
-
-        }
+  methods: {
+    getTableData() {
+      this.supplierSelectOptions = supplierModel.getSelectOptions();
+      this.loading = true;
+      let filters = [];
+      if (this.searchParam.skuCode != null && this.searchParam.skuCode !== "") {
+        filters.push({
+          field: "skuCode",
+          op: "equal",
+          data: `${this.searchParam.skuCode}`
+        });
       }
+      if (
+        this.searchParam.supplierId.value != null &&
+        this.searchParam.supplierId.value !== ""
+      ) {
+        filters.push({
+          field: "supplier_id",
+          op: "equal",
+          data: `${this.searchParam.supplierId.value}`
+        });
+      }
+      this.global
+        .axios({
+          header: {
+            "TK-Authorization":
+              "MUQ5RjMwRjcwMUE0NkUwRkUxNkUyMkNDNkZFNDNBOTEsMg=="
+          },
+          url: "/report/linerShippingPlanItemVOCount",
+          method: "GET",
+          params: {
+            currentPage: this.currentPage,
+            pageSize: this.pageSize,
+            filters: JSON.stringify(filters)
+          }
+        })
+        .then(res => {
+          this.total = res.data;
+        });
+
+      this.global
+        .axios({
+          header: {
+            "TK-Authorization":
+              "MUQ5RjMwRjcwMUE0NkUwRkUxNkUyMkNDNkZFNDNBOTEsMg=="
+          },
+          url: "/report/linerShippingPlanItemVOList",
+          method: "GET",
+          params: {
+            currentPage: this.currentPage,
+            pageSize: this.pageSize,
+            filters: JSON.stringify(filters)
+          }
+        })
+        .then(res => {
+          this.tableData = res.data;
+          this.loading = false;
+        });
     },
-    computed: {},
-
-    methods: {
-
+    reset() {
+      this.searchParam.skuCode = "";
+      this.getTableData();
     },
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.currentPage = 1;
+      this.getTableData();
+    },
+    // 当前页
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      this.getTableData();
+    },
+    onRefreshTable: function() {
+      this.getTableData();
+    }
+  },
 
-    // 观察data中的值发送变化后，调用
-    watch: {}
-  }
+  // 观察data中的值发送变化后，调用
+  watch: {}
+};
 </script>
 
-<style scoped>
+<style scoped type="text/less" lang="scss">
+.form-class {
+  padding: 0px 15px;
+  font-size: 12px;
+  position: relative;
+  border-bottom: 1px solid #f6f6f6;
+  color: #333;
+  border-radius: 2px 2px 0 0;
+  /* font-weight: bold !important;
+  line-height: 26px !important;
+  color: #333; */
+}
 
 </style>
