738d68143c65ed3098e805a9a6931fce7f44d549
tankai <tank@1ycloud.net>
2020-04-16 17:30:00 +0800

    重构进销存库存

---
+++ src/views/EndAndTerm/components/tab.vue
@@ -0,0 +1,29 @@
+<template>
+
+
+  <warehouseTable ref="warehouseTable" :type="type" :v-if="type=='warehouse'"></warehouseTable>
+
+</template>
+
+<script>
+  import warehouseTable from './warehouseTable'
+
+  export default {
+    components: {
+      warehouseTable
+    },
+    props: {
+      type: {
+        type: String,
+        default: 'warehouse'
+      }
+    },
+    computed: {},
+    methods: {
+      onRefreshTable() {
+        //this.$refs.warehouseTable.onRefreshTable();
+      }
+    }
+  }
+</script>
+
--- src/views/EndAndTerm/index.vue
+++ src/views/EndAndTerm/index.vue
@@ -1,224 +1,82 @@
 <template>
   <div class="app-container">
+
     <div class="ph-card">
       <div class="ph-card-body">
-        <ph-table
-          v-bind="tableConfig"
-        >
-        </ph-table>
+
+        <el-tabs v-model="activeStatus" type="border-card" @tab-click="handleTabClick">
+
+          <!-- TODO: name 根据实际情况修改  -->
+          <el-tab-pane name="warehouse" class="fontColor" lazy>
+            <span slot="label">
+              <i class="el-icon-s-home"></i> 仓库维度
+            </span>
+            <keep-alive>
+              <phTab type="warehouse" ref="warehouseTable"/>
+            </keep-alive>
+          </el-tab-pane>
+
+
+        </el-tabs>
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
-  import phColumns from '../../components/phColumns'
-  import warehouseModel from '../../api/warehouse'
-  import {checkPermission} from "../../utils/permission";
-  import currencyModel from "../../api/currency";
-  import cartonspecModel from "../../api/cartonspec";
-  import validRules from "../../components/validRules";
-  import moment from 'moment';
+  import phTab from './components/tab'
+  import {checkPermission} from "@/utils/permission";
+
+  const actionFlag = 's='
 
   export default {
+
+    components: {
+      phTab
+    },
+
     data() {
       return {
-        title: '出入库记录',
-        tableConfig: {
-          exportFileName: '出入库记录',
-          noExportProps: ['结算状态', 'ID'],
-          paginationSize: 50,
-          //权限控制
-          hasNew: false,
-          hasEdit: false,
-          hasDelete: false,
-          hasView: false,
-          hasExport: true,
+        // TODO 页面标题
+        title: '库存商品进销存报表',
 
-          url: '/report/findBeginEndOfPeriodList',
-          countUrl: null,
-          relations: [],
-          tableAttrs: {
-            "row-class-name": this.statusClassName
-          },
-          hasOperation: false,
-          hasNew: false,
-          hasDelete: false,
-          'default-sort': "{prop: 'createTime', order: 'descending'}",
-          //列表
-          columns: [
-            {prop: 'formatCreateTime', label: '创建时间', 'min-width': 140, fixed: 'left'},
-            {prop: 'warehouse.name', label: '收货仓库', 'min-width': 90},
-            {prop: 'code', label: '批次码', 'min-width': 130},
-            {prop: 'allocationCode', label: '调拨单', 'min-width': 130},
-            {prop: 'typeName', label: '类型', 'min-width': 100},
-            /*{prop: 'storageLocation.code', label: '存放货位', 'min-width': 100},*/
-            {prop: 'procurementOrder.company.abbreviation', label: '购买方', 'min-width': 100},
-            {prop: 'supplier.name', label: '供货商', 'min-width': 100},
-            {prop: 'product.skuCode', label: 'SKU编码', 'min-width': 150},
-            {prop: 'product.name', label: '产品名', 'min-width': 150},
-            {prop: 'numberOfCarton', label: '装箱数', 'min-width': 90},
+        // TODO 默认Tab激活状态
+        activeStatus: location.href.indexOf(actionFlag) > -1
+          ? (this.$route.query.s !== null ? this.$route.query.s : 'warehouse')
+          : 'warehouse',
+      }
+    },
 
-            {prop: 'currency.name', label: '结算货币', 'min-width': 100, hidden: !checkPermission('PurchasePriceVisible')},
-            phColumns.price,
-            phColumns.amount,
-            phColumns.jobStatus,
-            phColumns.id,
+    computed: {
+      hasNew() {
+        return checkPermission('ProcurementPlanResource_create');
+      }
+    },
 
-            {prop: 'cartonQty', label: '箱数', 'min-width': 90, fixed: 'right'},
-            {prop: 'qty', label: '件数', 'min-width': 90, fixed: 'right'},
-            {prop: 'usedQty', label: '消耗件数', 'min-width': 90, fixed: 'right'},
-
-
-          ],
-          // 搜索
-          searchForm: [
-            {
-              $type: 'select',
-              $id: 'warehouseId',
-              label: '仓库',
-              $options: warehouseModel.getSelectDomesticAndMaterialOptions(),
-              $el: {
-                op: 'eq',
-                style: 'width:160px',
-                filterable: true,
-                size: "mini",
-                placeholder: '请选择仓库'
-              }
-            },
-            {
-              $type: 'date-picker',
-              $id: 'reportDate',
-              label: '日期',
-              $default: `${moment(new Date()).startOf('month').format("YYYY-MM-DD")}|${moment(new Date()).endOf('month').format("YYYY-MM-DD")}`,
-              $el: {
-                style: 'width:220px',
-                op: 'timeRange',
-                size: "mini",
-                format: 'yyyy-MM-dd',
-                'value-format': 'yyyy-MM-dd',
-                type: 'daterange',
-                'range-separator': '|',
-                'start-placeholder': '开始日期',
-                'end-placeholder': '结束日期'
-              }
-            },
-            {
-              $type: 'input',
-              $id: 'skuCode',
-              label: 'SKU编码',
-              $el: {
-                style: 'width:160px',
-                op: 'eq',
-                size: "mini",
-                placeholder: '请输入产品SKU编码'
-              }
-            },
-            {
-              $type: 'input',
-              $id: 'productName',
-              label: '产品名',
-              $el: {
-                style: 'width:160px',
-                op: 'eq',
-                size: "mini",
-                placeholder: '请输入产品名'
-              }
-            },
-          ],
-          //修改或新增
-          form: [
-            {
-              $type: 'select',
-              $id: 'currencyId',
-              label: '货币',
-              $el: {
-                filterable: true,
-                placeholder: '请选择货币,可筛选',
-                size: 'mini'
-              },
-              $options: currencyModel.getSelectOptions(),
-              rules: [
-                validRules.required
-              ]
-            },
-            {
-              $type: 'input',
-              $id: 'price',
-              label: '采购价',
-              $el: {
-                placeholder: '请输入采购价'
-              },
-              rules: [
-                validRules.required,
-                validRules.number
-              ]
-            },
-            {
-              $type: 'select',
-              $id: 'cartonSpecId',
-              label: '箱规',
-              $el: {
-                filterable: true,
-                placeholder: '请选择箱规,可筛选',
-                size: 'mini'
-              },
-              $options: cartonspecModel.getSelectOptions(),
-              rules: [
-                validRules.required
-              ]
-            },
-            {
-              $type: 'input',
-              $id: 'numberOfCarton',
-              label: '装箱数',
-              $el: {
-                placeholder: '请输入装箱数'
-              },
-              rules: [
-                validRules.required,
-                validRules.integer
-              ]
-            },
-            {
-              $type: 'input',
-              $id: 'qty',
-              label: '件数',
-              $el: {
-                placeholder: '请输入装箱数'
-              },
-              rules: [
-                validRules.required,
-                validRules.integer
-              ]
-            },
-            {
-              $type: 'input',
-              $id: 'usedQty',
-              label: '冲销数量',
-              $el: {
-                placeholder: '请输入冲销数量'
-              },
-              rules: [
-                validRules.required,
-                validRules.integer
-              ]
-            },
-          ]
+    // 各种相关方法定义
+    methods: {
+      /* 点击Tag相应事件 */
+      // TODO: 通过URL记录点击Tab，方便刷新后不会切换视图
+      handleTabClick(tab, event) {
+        const queryFlag = '?s=';
+        const queryPath = '/m10/EndAndTerm_index';
+        let newUrl = location.origin + "/#" + queryPath + queryFlag + this.activeStatus;
+        history.pushState(history.state, 'ph-table search', newUrl);
+      },
+      /* 创建成功之后回调，刷新草稿状态列表列表 TODO: */
+      createCBEvent(objectId) {
+        if (objectId) {
+          if (this.$refs.editTable) {
+            this.$refs.editTable.onRefreshTable();
+          }
         }
       }
     },
-    computed: {},
-    methods: {
-      statusClassName({row}) {
-        return '';
-      },
-    },
-    watch: {}
+
   }
 </script>
 
-<style scoped>
-
+<style type="text/less" lang="scss" scoped>
 
 </style>
