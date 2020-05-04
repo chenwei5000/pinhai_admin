ebcbf9716ed09976cc9369a01de558e097f699f6
tankai <tank@1ycloud.net>
2020-05-04 18:28:37 +0800

    调整页面

--- src/views/FinanceBill/index.vue
+++ src/views/FinanceBill/index.vue
@@ -95,5 +95,8 @@
 </script>
 
 <style type="text/less" lang="scss" scoped>
+  .ph-table {
+    padding: 0 !important;
+  }
 
 </style>
--- src/views/Inventory/index.vue
+++ src/views/Inventory/index.vue
@@ -112,5 +112,7 @@
 </script>
 
 <style type="text/less" lang="scss" scoped>
-
+  .ph-table {
+    padding: 0 !important;
+  }
 </style>
